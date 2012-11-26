5dead718d479148a74cc8e41fc65cfbe0064b79b
Brian Stephanik <xoedusk@gmail.com>
2012-11-26 00:42:38 -0800

    Finished problem 17.

--- solutions/p17.py
+++ solutions/p17.py
@@ -14,14 +14,16 @@
 # British usage.
 
 # ALGORITHM
-# Two useful functions are defined. One function converts an integer to
-# a string of that integer in words (e.g., 4 -> four). Another function
-# counts the number of alphabetic characters in a string. As for the algorithm,
-# each number 1 through 1000 is converted into words. These words are run
-# through the second function above and the running total calculated.
+# One very useful class is defined. This class is able, among other things,
+# to convert an int to a string of that integer written out. For example,
+# the integer 38 would become 'thirty-eight'. Addition of the objects is
+# also supported.
 #
 # In the interest of reusability, numbers up to 1e12 - 1 (one less than one
 # trillion) are converted to words.
+
+from solutionTimer import start, stop
+import re
 
 base_ints_to_words = {1: 'one',
                     2: 'two',
@@ -50,7 +52,6 @@ base_ints_to_words = {1: 'one',
                     70: 'seventy',
                     80: 'eighty',
                     90: 'ninety'}
-                    #100: 'hundred'}
 
 powers_of_ten = ['', 'thousand', 'million', 'billion', 'trillion']
 
@@ -58,11 +59,7 @@ class NumericWord(object):
     __MAX_NUMBER = int(1e12-1)
     def __init__(self, num, extraAnds=False):
         '''NumericWord(num) -> new NumericWord object using the integer num'''
-        NumericWord.check_max_number(num) # Raise exception of number is too big
-        if not num >= 0:
-            raise ValueError, 'Argument num must be a non-negative integer.'
-        if not num <= NumericWord.__MAX_NUMBER:
-            raise ValueError, 'Argument num must be less than or equal to', __MAX_NUMBER
+        NumericWord.check_max_number(num) # Is num too big/small?
         
         self.__numAsInt = num
         self.__extraAnds = extraAnds
@@ -77,14 +74,31 @@ class NumericWord(object):
     def __add__(self, other):
         if isinstance(other, int):
             new_num = self.__numAsInt + other
-        new_num = self.__numAsInt + other.__numAsInt
+        else:
+            new_num = self.__numAsInt + other.__numAsInt
         return NumericWord(new_num, extraAnds=self.__extraAnds)
+    
+    def __sub__(self, other):
+        if isinstance(other, int):
+            new_num = self.__numAsInt - other
+        else:
+            new_num = self.__numAsInt - other.__numAsInt
+        return NumericWord(new_num, extraAnds=self.__extraAnds)
+    
+    def __cmp__(self, other):
+        return self.__numAsInt - other.__numAsInt
+    
+    def __repr__(self):
+        return 'p17.NumericWord(%d, extraAnds=%s)' % (self.__numAsInt, self.__extraAnds)
+    
     
     @staticmethod
     def check_max_number(num):
-        if num > NumericWord.__MAX_NUMBER:
-            raise ValueError, 'Argument num must be less than ' + \
-                str(NumericWord.__MAX_NUMBER)
+        if not isinstance(num, int):
+            raise TypeError, 'Argument num must be an integer.'
+        if num < -NumericWord.__MAX_NUMBER or num > NumericWord.__MAX_NUMBER:
+            raise ValueError, 'Argument num must be between '+ \
+                str(-NumericWord.__MAX_NUMBER) + ' and ' + str(NumericWord.__MAX_NUMBER)
     
     @staticmethod
     def word(num, extraAnds=False):
@@ -99,6 +113,13 @@ class NumericWord(object):
         
         '''
         NumericWord.check_max_number(num)
+        if num == 0:
+            return 'zero'
+        if num < 0:
+            negative = True
+            num *= -1
+        else:
+            negative = False
         # Convert num from 25311 to ['25', '311'], for example.
         grouped_digit_list = []
         int_str = str(num) # int_str == '25311'
@@ -115,42 +136,50 @@ class NumericWord(object):
         grouped_digit_words = [] # Will hold ['twenty-five', 'three hundred eleven']
         for dig in grouped_digit_list: # dig is a string, like '532' or '1'
             dig_int = int(dig)
-            try: # Assume int(dig) as already in base_ints_to_words
-                grouped_digit_words.append(base_ints_to_words[dig_int])
-            except KeyError: # dig_int was not already in base_ints_to_words
-                if dig_int == 0:
-                    grouped_digit_words.append('')
+            if dig_int == 0:
+                grouped_digit_words.append('')
+                continue
+            new_str = '' # Need to do actual work
+            if len(dig) == 3 and dig[0] != '0': # Have to deal with hundreds
+                new_str += base_ints_to_words[int(dig[0])] + ' hundred '
+                if dig[-2:] == '00': # Are we done with this group?
+                    grouped_digit_words.append(new_str[:-1]) # Get rid of space
                     continue
-                new_str = '' # Need to do to actual work
-                if len(dig) == 3 and dig[0] is not '0': # Deal with a three-group
-                    new_str += base_ints_to_words[int(dig[0])] + ' hundred '
-                    if dig[-2:] == '00': # Is there more to say after hundreds?
-                        grouped_digit_words.append(new_str[:-1])
-                        continue
-                if extraAnds and len(dig) == 3 : # British?
-                    new_str += 'and '
-                # Now deal with the tens and ones
-                try:
-                    new_str += base_ints_to_words[int(dig[-2:])]
-                    grouped_digit_words.append(new_str)
-                    continue
-                except KeyError:
-                    if dig[-2] is not '0':
-                        new_str += base_ints_to_words[int(dig[-2])*10]
-                    if dig[-1] is not '0':
-                        if dig[-2] is not '0': # The teens have already been dealt with
-                            new_str += '-'
-                        new_str += base_ints_to_words[int(dig[-1])] + ' '
+            if extraAnds and len(dig) == 3: # British
+                new_str += 'and '
+            # Ready to deal with the last two digits
+            try: # One of the 'special' numbers in base_ints_to_words?
+                new_str += base_ints_to_words[int(dig[-2:])]
+                grouped_digit_words.append(new_str)
+                continue
+            except KeyError: # Not a 'special' number in base_ints_to_words
+                new_str += base_ints_to_words[int(dig[-2])*10]
+                new_str += '-'
+                new_str += base_ints_to_words[int(dig[-1])]
+                new_str += ' '
                 grouped_digit_words.append(new_str[:-1])
         
         tens_idx = 0
         number_groups = []
         for words in grouped_digit_words[-1::-1]:
-            if words is not '':
-                complete_group = words + ' ' + powers_of_ten[tens_idx]
+            if words != '':
+                complete_group = words
+                if powers_of_ten[tens_idx] != '':
+                    complete_group += ' ' + powers_of_ten[tens_idx]
                 number_groups.insert(0,complete_group)
             tens_idx += 1
-        #number_groups.append(grouped_digit_words[-1])
         
         num_as_str = ' '.join(number_groups)
+        if negative:
+            num_as_str = 'negative ' + num_as_str
         return num_as_str
+
+if __name__ == '__main__':
+    start()
+    count = 0
+    for num in range(1, 1000 + 1):
+        num_str = NumericWord.word(num, extraAnds=True)
+        print num_str
+        count += len(re.findall(r'[a-z]', NumericWord.word(num, extraAnds=True)))
+    print 'Problem 17 ->', count
+    stop()
--- solutions/tests/test_p17.py
+++ solutions/tests/test_p17.py
@@ -6,8 +6,8 @@ from p17 import *
 import unittest
 
 class IntToWordsTest(unittest.TestCase):
-    def test_known_values(self):
-        #self.assertEqual(NumericWord.word(0, extraAnds=True), 'zero')
+    def test_known_values_british(self):
+        self.assertEqual(NumericWord.word(0, extraAnds=True), 'zero')
         self.assertEqual(NumericWord.word(1, extraAnds=True), 'one')
         self.assertEqual(NumericWord.word(2, extraAnds=True), 'two')
         self.assertEqual(NumericWord.word(3, extraAnds=True), 'three')
@@ -159,7 +159,7 @@ class IntToWordsTest(unittest.TestCase):
         self.assertEqual(NumericWord.word(3000300, extraAnds=True),
                          'three million three hundred')
         self.assertEqual(NumericWord.word(4004000, extraAnds=True),
-                         'four million four thousand')
+                         'four million and four thousand')
         self.assertEqual(NumericWord.word(5050000, extraAnds=True),
                          'five million and fifty thousand')
         self.assertEqual(NumericWord.word(8800309, extraAnds=True),
@@ -183,18 +183,204 @@ class IntToWordsTest(unittest.TestCase):
         self.assertEqual(NumericWord.word(15630383711, extraAnds=True),
                          'fifteen billion six hundred and thirty million three hundred and eighty-three thousand seven hundred and eleven')
         self.assertEqual(NumericWord.word(980059103860, extraAnds=True),
-                         'nine hundred and eighty billion fifty-nine million one hundred and three thousand eight hundred and sixty')
-
-                         
-                         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+                         'nine hundred and eighty billion and fifty-nine million one hundred and three thousand eight hundred and sixty')
+    
+    def test_known_values_non_british(self):
+        self.assertEqual(NumericWord.word(0, extraAnds=False), 'zero')
+        self.assertEqual(NumericWord.word(1, extraAnds=False), 'one')
+        self.assertEqual(NumericWord.word(2, extraAnds=False), 'two')
+        self.assertEqual(NumericWord.word(3, extraAnds=False), 'three')
+        self.assertEqual(NumericWord.word(4, extraAnds=False), 'four')
+        self.assertEqual(NumericWord.word(5, extraAnds=False), 'five')
+        self.assertEqual(NumericWord.word(6, extraAnds=False), 'six')
+        self.assertEqual(NumericWord.word(7, extraAnds=False), 'seven')
+        self.assertEqual(NumericWord.word(8, extraAnds=False), 'eight')
+        self.assertEqual(NumericWord.word(9, extraAnds=False), 'nine')
+        self.assertEqual(NumericWord.word(10, extraAnds=False), 'ten')
+        self.assertEqual(NumericWord.word(11, extraAnds=False), 'eleven')
+        self.assertEqual(NumericWord.word(12, extraAnds=False), 'twelve')
+        self.assertEqual(NumericWord.word(13, extraAnds=False), 'thirteen')
+        self.assertEqual(NumericWord.word(14, extraAnds=False), 'fourteen')
+        self.assertEqual(NumericWord.word(15, extraAnds=False), 'fifteen')
+        self.assertEqual(NumericWord.word(16, extraAnds=False), 'sixteen')
+        self.assertEqual(NumericWord.word(17, extraAnds=False), 'seventeen')
+        self.assertEqual(NumericWord.word(18, extraAnds=False), 'eighteen')
+        self.assertEqual(NumericWord.word(19, extraAnds=False), 'nineteen')
+        self.assertEqual(NumericWord.word(20, extraAnds=False), 'twenty')
+        self.assertEqual(NumericWord.word(21, extraAnds=False), 'twenty-one')
+        self.assertEqual(NumericWord.word(22, extraAnds=False), 'twenty-two')
+        self.assertEqual(NumericWord.word(23, extraAnds=False), 'twenty-three')
+        self.assertEqual(NumericWord.word(24, extraAnds=False), 'twenty-four')
+        self.assertEqual(NumericWord.word(25, extraAnds=False), 'twenty-five')
+        self.assertEqual(NumericWord.word(26, extraAnds=False), 'twenty-six')
+        self.assertEqual(NumericWord.word(27, extraAnds=False), 'twenty-seven')
+        self.assertEqual(NumericWord.word(28, extraAnds=False), 'twenty-eight')
+        self.assertEqual(NumericWord.word(29, extraAnds=False), 'twenty-nine')
+        self.assertEqual(NumericWord.word(30, extraAnds=False), 'thirty')
+        self.assertEqual(NumericWord.word(31, extraAnds=False), 'thirty-one')
+        self.assertEqual(NumericWord.word(40, extraAnds=False), 'forty')
+        self.assertEqual(NumericWord.word(41, extraAnds=False), 'forty-one')
+        self.assertEqual(NumericWord.word(50, extraAnds=False), 'fifty')
+        self.assertEqual(NumericWord.word(51, extraAnds=False), 'fifty-one')
+        self.assertEqual(NumericWord.word(60, extraAnds=False), 'sixty')
+        self.assertEqual(NumericWord.word(61, extraAnds=False), 'sixty-one')
+        self.assertEqual(NumericWord.word(70, extraAnds=False), 'seventy')
+        self.assertEqual(NumericWord.word(71, extraAnds=False), 'seventy-one')
+        self.assertEqual(NumericWord.word(80, extraAnds=False), 'eighty')
+        self.assertEqual(NumericWord.word(81, extraAnds=False), 'eighty-one')
+        self.assertEqual(NumericWord.word(90, extraAnds=False), 'ninety')
+        self.assertEqual(NumericWord.word(91, extraAnds=False), 'ninety-one')
+        self.assertEqual(NumericWord.word(100, extraAnds=False), 'one hundred')
+        self.assertEqual(NumericWord.word(101, extraAnds=False),
+                         'one hundred one')
+        self.assertEqual(NumericWord.word(102, extraAnds=False),
+                         'one hundred two')
+        self.assertEqual(NumericWord.word(109, extraAnds=False),
+                         'one hundred nine')
+        self.assertEqual(NumericWord.word(110, extraAnds=False),
+                         'one hundred ten')
+        self.assertEqual(NumericWord.word(111, extraAnds=False),
+                         'one hundred eleven')
+        self.assertEqual(NumericWord.word(120, extraAnds=False),
+                         'one hundred twenty')
+        self.assertEqual(NumericWord.word(121, extraAnds=False),
+                         'one hundred twenty-one')
+        self.assertEqual(NumericWord.word(130, extraAnds=False),
+                         'one hundred thirty')
+        self.assertEqual(NumericWord.word(131, extraAnds=False),
+                         'one hundred thirty-one')
+        self.assertEqual(NumericWord.word(199, extraAnds=False),
+                         'one hundred ninety-nine')
+        self.assertEqual(NumericWord.word(200, extraAnds=False),
+                         'two hundred')
+        self.assertEqual(NumericWord.word(201, extraAnds=False),
+                         'two hundred one')
+        self.assertEqual(NumericWord.word(240, extraAnds=False),
+                         'two hundred forty')
+        self.assertEqual(NumericWord.word(255, extraAnds=False),
+                         'two hundred fifty-five')
+        self.assertEqual(NumericWord.word(303, extraAnds=False),
+                         'three hundred three')
+        self.assertEqual(NumericWord.word(404, extraAnds=False),
+                         'four hundred four')
+        self.assertEqual(NumericWord.word(505, extraAnds=False),
+                         'five hundred five')
+        self.assertEqual(NumericWord.word(606, extraAnds=False),
+                         'six hundred six')
+        self.assertEqual(NumericWord.word(707, extraAnds=False),
+                         'seven hundred seven')
+        self.assertEqual(NumericWord.word(808, extraAnds=False),
+                         'eight hundred eight')
+        self.assertEqual(NumericWord.word(909, extraAnds=False),
+                         'nine hundred nine')
+        self.assertEqual(NumericWord.word(999, extraAnds=False),
+                         'nine hundred ninety-nine')
+        self.assertEqual(NumericWord.word(1000, extraAnds=False),
+                         'one thousand')
+        self.assertEqual(NumericWord.word(1001, extraAnds=False),
+                         'one thousand one')
+        self.assertEqual(NumericWord.word(1111, extraAnds=False),
+                         'one thousand one hundred eleven')
+        self.assertEqual(NumericWord.word(2222, extraAnds=False),
+                         'two thousand two hundred twenty-two')
+        self.assertEqual(NumericWord.word(3333, extraAnds=False),
+                         'three thousand three hundred thirty-three')
+        self.assertEqual(NumericWord.word(4444, extraAnds=False),
+                         'four thousand four hundred forty-four')
+        self.assertEqual(NumericWord.word(5555, extraAnds=False),
+                         'five thousand five hundred fifty-five')
+        self.assertEqual(NumericWord.word(6666, extraAnds=False),
+                         'six thousand six hundred sixty-six')
+        self.assertEqual(NumericWord.word(7777, extraAnds=False),
+                         'seven thousand seven hundred seventy-seven')
+        self.assertEqual(NumericWord.word(8888, extraAnds=False),
+                         'eight thousand eight hundred eighty-eight')
+        self.assertEqual(NumericWord.word(9999, extraAnds=False),
+                         'nine thousand nine hundred ninety-nine')
+        self.assertEqual(NumericWord.word(10000, extraAnds=False),
+                         'ten thousand')
+        self.assertEqual(NumericWord.word(10008, extraAnds=False),
+                         'ten thousand eight')
+        self.assertEqual(NumericWord.word(10632, extraAnds=False),
+                         'ten thousand six hundred thirty-two')
+        self.assertEqual(NumericWord.word(11611, extraAnds=False),
+                         'eleven thousand six hundred eleven')
+        self.assertEqual(NumericWord.word(11999, extraAnds=False),
+                         'eleven thousand nine hundred ninety-nine')
+        self.assertEqual(NumericWord.word(12190, extraAnds=False),
+                         'twelve thousand one hundred ninety')
+        self.assertEqual(NumericWord.word(25000, extraAnds=False),
+                         'twenty-five thousand')
+        self.assertEqual(NumericWord.word(25006, extraAnds=False),
+                         'twenty-five thousand six')
+        self.assertEqual(NumericWord.word(25030, extraAnds=False),
+                         'twenty-five thousand thirty')
+        self.assertEqual(NumericWord.word(25400, extraAnds=False),
+                         'twenty-five thousand four hundred')
+        self.assertEqual(NumericWord.word(99999, extraAnds=False),
+                         'ninety-nine thousand nine hundred ninety-nine')
+        self.assertEqual(NumericWord.word(100000, extraAnds=False),
+                         'one hundred thousand')
+        self.assertEqual(NumericWord.word(100001, extraAnds=False),
+                         'one hundred thousand one')
+        self.assertEqual(NumericWord.word(195366, extraAnds=False),
+                         'one hundred ninety-five thousand three hundred sixty-six')
+        self.assertEqual(NumericWord.word(240609, extraAnds=False),
+                         'two hundred forty thousand six hundred nine')
+        self.assertEqual(NumericWord.word(903011, extraAnds=False),
+                         'nine hundred three thousand eleven')
+        self.assertEqual(NumericWord.word(int(1e6), extraAnds=False),
+                         'one million')
+        self.assertEqual(NumericWord.word(int(1e6 + 1), extraAnds=False),
+                         'one million one')
+        self.assertEqual(NumericWord.word(2000020, extraAnds=False),
+                         'two million twenty')
+        self.assertEqual(NumericWord.word(3000300, extraAnds=False),
+                         'three million three hundred')
+        self.assertEqual(NumericWord.word(4004000, extraAnds=False),
+                         'four million four thousand')
+        self.assertEqual(NumericWord.word(5050000, extraAnds=False),
+                         'five million fifty thousand')
+        self.assertEqual(NumericWord.word(8800309, extraAnds=False),
+                         'eight million eight hundred thousand three hundred nine')
+        self.assertEqual(NumericWord.word(10000000, extraAnds=False),
+                         'ten million')
+        self.assertEqual(NumericWord.word(10000411, extraAnds=False),
+                         'ten million four hundred eleven')
+        self.assertEqual(NumericWord.word(10203040, extraAnds=False),
+                         'ten million two hundred three thousand forty')
+        self.assertEqual(NumericWord.word(52333000, extraAnds=False),
+                         'fifty-two million three hundred thirty-three thousand')
+        self.assertEqual(NumericWord.word(700800208, extraAnds=False),
+                         'seven hundred million eight hundred thousand two hundred eight')
+        self.assertEqual(NumericWord.word(999999999, extraAnds=False),
+                         'nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine')
+        self.assertEqual(NumericWord.word(1000000000, extraAnds=False),
+                         'one billion')
+        self.assertEqual(NumericWord.word(8631911155, extraAnds=False),
+                         'eight billion six hundred thirty-one million nine hundred eleven thousand one hundred fifty-five')
+        self.assertEqual(NumericWord.word(15630383711, extraAnds=False),
+                         'fifteen billion six hundred thirty million three hundred eighty-three thousand seven hundred eleven')
+        self.assertEqual(NumericWord.word(980059103860, extraAnds=False),
+                         'nine hundred eighty billion fifty-nine million one hundred three thousand eight hundred sixty')
+    def test_negative_numbers(self):
+        for n in range(-1, -2000, -1):
+            for ands in [True, False]:
+                self.assertEqual(NumericWord.word(n, extraAnds=ands), 'negative ' + NumericWord.word(-n, extraAnds=ands))
+    
+    def test_num_too_big(self):
+        self.assertRaises(ValueError, NumericWord.word, int(1e12))
+        self.assertRaises(ValueError, NumericWord.word, int(-1e12))
+    
+    def test_bad_type(self):
+        self.assertRaises(TypeError, NumericWord.word, 6.0)
+    
+    def test_add(self):
+        for x in range(-25, 25):
+            for y in range(-25, 25):
+                self.assertEqual(NumericWord(x + y), NumericWord(x) + NumericWord(y))
+    
+    def test_sub(self):
+        for x in range(-25, 25):
+            for y in range(-25, 25):
+                self.assertEqual(NumericWord(x - y), NumericWord(x) - NumericWord(y))
